f80e94e4011ebe6a279122d107282d497d62221e
Dmitry Conquer <dmitry.conquer@gmail.com>
2023-11-15 19:13:47 +0200

    header, hero, pros

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: ["./src/scss/**/*.scss", "./src/**/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      '2xl': "1440px"
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#B3CBE4",
+          100: "#E5F4FC",
+          200: "#446B80", // text
+          300: "#223540", // dark
+        },
+        secondary: {
+          DEFAULT: "#ECC7C1",
+          100: "#FDF6EF",
+          200: "#F3DBD7",
+          300: "#d48073",
+        },
+      },
+    },
+    fontFamily: {
+      balsamiq: ["Balsamiq Sans", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
